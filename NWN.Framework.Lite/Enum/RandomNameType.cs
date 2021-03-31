979cdf7e5a1b375a3cd532bbb7076a43c00a8e0e
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-30 21:16:58 -0700

    RandomNameType

--- NWN.Framework.Lite/Enum/RandomNameType.cs
+++ NWN.Framework.Lite/Enum/RandomNameType.cs
@@ -1,31 +1,34 @@
 namespace NWN.Framework.Lite.Enum
 {
-    public enum RandomNameType
+    public class RandomNameType : NWEnumBase<int>
     {
         // These constants determine which name table to use when generating random names.
-        FirstGenericMale = -1,
-        Animal = 0,
-        Familiar = 1,
-        FirstDwarfMale = 2,
-        FirstDwarfFemale = 3,
-        LastDwarf = 4,
-        FirstElfMale = 5,
-        FirstElfFemale = 6,
-        LastElf = 7,
-        FirstGnomeMale = 8,
-        FirstGnomeFemale = 9,
-        LastGnome = 10,
-        FirstHalfelfMale = 11,
-        FirstHalfElfFemale = 12,
-        LastHalfElf = 13,
-        FirstHalflingMale = 14,
-        FirstHalflingFemale = 15,
-        LastHalfling = 16,
-        FirstHalfOrcMale = 17,
-        FirstHalfOrcFemale = 18,
-        LastHalfOrc = 19,
-        FirstHumanMale = 20,
-        FirstHumanFemale = 21,
-        LastHuman = 22
+
+        public static readonly RandomNameType FirstGenericMale = new(-1);
+        public static readonly RandomNameType Animal = new(0);
+        public static readonly RandomNameType Familiar = new(1);
+        public static readonly RandomNameType FirstDwarfMale = new(2);
+        public static readonly RandomNameType FirstDwarfFemale = new(3);
+        public static readonly RandomNameType LastDwarf = new(4);
+        public static readonly RandomNameType FirstElfMale = new(5);
+        public static readonly RandomNameType FirstElfFemale = new(6);
+        public static readonly RandomNameType LastElf = new(7);
+        public static readonly RandomNameType FirstGnomeMale = new(8);
+        public static readonly RandomNameType FirstGnomeFemale = new(9);
+        public static readonly RandomNameType LastGnome = new(10);
+        public static readonly RandomNameType FirstHalfelfMale = new(11);
+        public static readonly RandomNameType FirstHalfElfFemale = new(12);
+        public static readonly RandomNameType LastHalfElf = new(13);
+        public static readonly RandomNameType FirstHalflingMale = new(14);
+        public static readonly RandomNameType FirstHalflingFemale = new(15);
+        public static readonly RandomNameType LastHalfling = new(16);
+        public static readonly RandomNameType FirstHalfOrcMale = new(17);
+        public static readonly RandomNameType FirstHalfOrcFemale = new(18);
+        public static readonly RandomNameType LastHalfOrc = new(19);
+        public static readonly RandomNameType FirstHumanMale = new(20);
+        public static readonly RandomNameType FirstHumanFemale = new(21);
+        public static readonly RandomNameType LastHuman = new(22);
+
+        public RandomNameType(int internalValue) : base(internalValue) { }
     }
 }
