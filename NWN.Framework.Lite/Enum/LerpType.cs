9e4d93dcbf3d08c3792afbce9200974c422755f9
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-31 00:22:21 -0700

    LerpType

--- NWN.Framework.Lite/Enum/LerpType.cs
+++ NWN.Framework.Lite/Enum/LerpType.cs
@@ -1,15 +1,16 @@
 ﻿namespace NWN.Framework.Lite.Enum
 {
-    public enum LerpType
+    public class LerpType : NWEnumBase<int>
     {
-        None = 0, // 1
-        Linear = 1, // x
-        Smoothstep = 2, // x * x * (3 - 2 * x)
-        InverseSmoothstep = 3, // 0.5 - sin(asin(1.0 - 2.0 * x) / 3.0)
-        EaseIn = 4, // (1 - cosf(x * M_PI * 0.5))
-        EaseOut = 5, // sinf(x * M_PI * 0.5)
-        Quadratic = 6, // x * x
-        SmootherStep = 7 // (x * x * x * (x * (6.0 * x - 15.0) + 10.0))
+        public static readonly LerpType None = new(0); // 1
+        public static readonly LerpType Linear = new(1); // x
+        public static readonly LerpType Smoothstep = new(2); // x * x * (3 - 2 * x)
+        public static readonly LerpType InverseSmoothstep = new(3); // 0.5 - sin(asin(1.0 - 2.0 * x) / 3.0)
+        public static readonly LerpType EaseIn = new(4); // (1 - cosf(x * M_PI * 0.5))
+        public static readonly LerpType EaseOut = new(5); // sinf(x * M_PI * 0.5)
+        public static readonly LerpType Quadratic = new(6); // x * x
+        public static readonly LerpType SmootherStep = new(7); // (x * x * x * (x * (6.0 * x - 15.0) + 10.0))
 
+        public LerpType(int internalValue) : base(internalValue) { }
     }
 }
