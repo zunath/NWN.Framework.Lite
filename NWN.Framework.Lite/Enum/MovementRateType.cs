5d74a56f0c7a44193db2f6af540b81afc662c681
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-31 00:11:44 -0700

    MovementRateType

--- NWN.Framework.Lite/Enum/MovementRateType.cs
+++ NWN.Framework.Lite/Enum/MovementRateType.cs
@@ -1,15 +1,17 @@
 ﻿namespace NWN.Framework.Lite.Enum
 {
-    public enum MovementRateType
+    public class MovementRateType : NWEnumBase<int>
     {
-        PC,
-        Immobile,
-        VerySlow,
-        Slow,
-        Normal,
-        Fast,
-        VeryFast,
-        Default,
-        DMFast
+        public static readonly MovementRateType PC = new(0);
+        public static readonly MovementRateType Immobile = new(1);
+        public static readonly MovementRateType VerySlow = new(2);
+        public static readonly MovementRateType Slow = new(3);
+        public static readonly MovementRateType Normal = new(4);
+        public static readonly MovementRateType Fast = new(5);
+        public static readonly MovementRateType VeryFast = new(6);
+        public static readonly MovementRateType Default = new(7);
+        public static readonly MovementRateType DMFast = new(8);
+
+        public MovementRateType(int internalValue) : base(internalValue) { }
     }
 }
