ea89f67192a3bcf0de083c1e0acb1394f27f4c8e
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-30 23:56:41 -0700

    ObjectTypeEngine

--- NWN.Framework.Lite/Enum/ObjectTypeEngine.cs
+++ NWN.Framework.Lite/Enum/ObjectTypeEngine.cs
@@ -1,23 +1,25 @@
 namespace NWN.Framework.Lite.Enum
 {
-    public enum ObjectTypeEngine
+    public class ObjectTypeEngine : NWEnumBase<int>
     {
-        Invalid = -1,
-        GUI = 1,
-        Tile,
-        Module,
-        Area,
-        Creature,
-        Item,
-        Trigger,
-        Projectile,
-        Placeable,
-        Door,
-        AreaOfEffect,
-        Waypoint,
-        Encounter,
-        Store,
-        Portal,
-        Sound
+        public static readonly ObjectTypeEngine Invalid = new(-1);
+        public static readonly ObjectTypeEngine GUI = new(1);
+        public static readonly ObjectTypeEngine Tile = new(2);
+        public static readonly ObjectTypeEngine Module = new(3);
+        public static readonly ObjectTypeEngine Area = new(4);
+        public static readonly ObjectTypeEngine Creature = new(5);
+        public static readonly ObjectTypeEngine Item = new(6);
+        public static readonly ObjectTypeEngine Trigger = new(7);
+        public static readonly ObjectTypeEngine Projectile = new(8);
+        public static readonly ObjectTypeEngine Placeable = new(9);
+        public static readonly ObjectTypeEngine Door = new(10);
+        public static readonly ObjectTypeEngine AreaOfEffect = new(11);
+        public static readonly ObjectTypeEngine Waypoint = new(12);
+        public static readonly ObjectTypeEngine Encounter = new(13);
+        public static readonly ObjectTypeEngine Store = new(14);
+        public static readonly ObjectTypeEngine Portal = new(15);
+        public static readonly ObjectTypeEngine Sound = new(16);
+
+        public ObjectTypeEngine(int internalValue) : base(internalValue) { }
     }
 }
