352eb18513583e3f15269dda7499b7adb9d4d995
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-31 00:17:58 -0700

    MetaMagicType

--- NWN.Framework.Lite/Enum/MetaMagicType.cs
+++ NWN.Framework.Lite/Enum/MetaMagicType.cs
@@ -1,13 +1,15 @@
 namespace NWN.Framework.Lite.Enum
 {
-    public enum MetaMagicType
+    public class MetaMagicType : NWEnumBase<int>
     {
-        None = 0,
-        Empower = 1,
-        Extend = 2,
-        Maximize = 4,
-        Quicken = 8,
-        Silent = 16,
-        Any = 255
+        public static readonly MetaMagicType None = new(0);
+        public static readonly MetaMagicType Empower = new(1);
+        public static readonly MetaMagicType Extend = new(2);
+        public static readonly MetaMagicType Maximize = new(4);
+        public static readonly MetaMagicType Quicken = new(8);
+        public static readonly MetaMagicType Silent = new(16);
+        public static readonly MetaMagicType Any = new(255);
+
+        public MetaMagicType(int internalValue) : base(internalValue) { }
     }
 }
