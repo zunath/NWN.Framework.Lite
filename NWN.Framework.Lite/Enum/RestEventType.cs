82e571c00defa91146481f5e00fe415be8593fce
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-30 21:06:57 -0700

    RestEventType

--- NWN.Framework.Lite/Enum/RestEventType.cs
+++ NWN.Framework.Lite/Enum/RestEventType.cs
@@ -1,10 +1,12 @@
 namespace NWN.Framework.Lite.Enum
 {
-    public enum RestEventType
+    public class RestEventType : NWEnumBase<int>
     {
-        Invalid,
-        Started,
-        Finished,
-        Cancelled
+        public static readonly RestEventType Invalid = new(0);
+        public static readonly RestEventType Started = new(1);
+        public static readonly RestEventType Finished = new(2);
+        public static readonly RestEventType Cancelled = new(3);
+
+        public RestEventType(int internalValue) : base(internalValue) { }
     }
 }
