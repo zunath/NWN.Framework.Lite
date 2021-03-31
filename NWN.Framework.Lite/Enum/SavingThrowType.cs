d11a8352d71eee5962d55b1396e05eeba5d3d8c2
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-30 21:00:28 -0700

    SavingThrowType

--- NWN.Framework.Lite/Enum/SavingThrowType.cs
+++ NWN.Framework.Lite/Enum/SavingThrowType.cs
@@ -1,26 +1,28 @@
 namespace NWN.Framework.Lite.Enum
 {
-    public enum SavingThrowType
+    public class SavingThrowType : NWEnumBase<int>
     {
-        All = 0,
-        MindSpells = 1,
-        Poison = 2,
-        Disease = 3,
-        Fear = 4,
-        Sonic = 5,
-        Acid = 6,
-        Fire = 7,
-        Electricity = 8,
-        Positive = 9,
-        Negative = 10,
-        Death = 11,
-        Cold = 12,
-        Divine = 13,
-        Trap = 14,
-        Spell = 15,
-        Good = 16,
-        Evil = 17,
-        Law = 18,
-        Chaos = 19
+        public static readonly SavingThrowType All = new(0);
+        public static readonly SavingThrowType MindSpells = new(1);
+        public static readonly SavingThrowType Poison = new(2);
+        public static readonly SavingThrowType Disease = new(3);
+        public static readonly SavingThrowType Fear = new(4);
+        public static readonly SavingThrowType Sonic = new(5);
+        public static readonly SavingThrowType Acid = new(6);
+        public static readonly SavingThrowType Fire = new(7);
+        public static readonly SavingThrowType Electricity = new(8);
+        public static readonly SavingThrowType Positive = new(9);
+        public static readonly SavingThrowType Negative = new(10);
+        public static readonly SavingThrowType Death = new(11);
+        public static readonly SavingThrowType Cold = new(12);
+        public static readonly SavingThrowType Divine = new(13);
+        public static readonly SavingThrowType Trap = new(14);
+        public static readonly SavingThrowType Spell = new(15);
+        public static readonly SavingThrowType Good = new(16);
+        public static readonly SavingThrowType Evil = new(17);
+        public static readonly SavingThrowType Law = new(18);
+        public static readonly SavingThrowType Chaos = new(19);
+
+        public SavingThrowType(int internalValue) : base(internalValue) { }
     }
 }
