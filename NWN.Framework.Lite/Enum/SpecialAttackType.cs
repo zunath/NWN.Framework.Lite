93c45139c3f2c26e0e2522746c52c806b7f9b307
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-30 20:19:23 -0700

    SpecialAttackType

--- NWN.Framework.Lite/Enum/SpecialAttackType.cs
+++ NWN.Framework.Lite/Enum/SpecialAttackType.cs
@@ -1,17 +1,19 @@
 namespace NWN.Framework.Lite.Enum
 {
-    public enum SpecialAttackType
+    public class SpecialAttackType : NWEnumBase<int>
     {
-        Invalid = 0,
-        CalledShotLeg = 1,
-        CalledShotArm = 2,
-        Sap = 3,
-        Disarm = 4,
-        ImprovedDisarm = 5,
-        Knockdown = 6,
-        ImprovedKnockdown = 7,
-        StunningFist = 8,
-        FlurryOfBlows = 9,
-        RapidShot = 10
+        public static readonly SpecialAttackType Invalid = new(0);
+        public static readonly SpecialAttackType CalledShotLeg = new(1);
+        public static readonly SpecialAttackType CalledShotArm = new(2);
+        public static readonly SpecialAttackType Sap = new(3);
+        public static readonly SpecialAttackType Disarm = new(4);
+        public static readonly SpecialAttackType ImprovedDisarm = new(5);
+        public static readonly SpecialAttackType Knockdown = new(6);
+        public static readonly SpecialAttackType ImprovedKnockdown = new(7);
+        public static readonly SpecialAttackType StunningFist = new(8);
+        public static readonly SpecialAttackType FlurryOfBlows = new(9);
+        public static readonly SpecialAttackType RapidShot = new(10);
+
+        public SpecialAttackType(int internalValue) : base(internalValue) { }
     }
 }
