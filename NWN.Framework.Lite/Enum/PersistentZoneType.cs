aa1ccb57a8af684ebe67b4d6a4689081c6e1f91a
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-30 23:46:13 -0700

    PersistentZoneType

--- NWN.Framework.Lite/Enum/PersistentZoneType.cs
+++ NWN.Framework.Lite/Enum/PersistentZoneType.cs
@@ -1,9 +1,11 @@
 namespace NWN.Framework.Lite.Enum
 {
     // These are for GetFirstInPersistentObject() and GetNextInPersistentObject()
-    public enum PersistentZoneType
+    public class PersistentZoneType : NWEnumBase<int>
     {
-        Active = 0,
-        Follow = 1
+        public static readonly PersistentZoneType Active = new(0);
+        public static readonly PersistentZoneType Follow = new(1);
+
+        public PersistentZoneType(int internalValue) : base(internalValue) { }
     }
 }
