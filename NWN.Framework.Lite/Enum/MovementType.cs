3687db0ad1d6f42f88bc31bab842b49616b51ae9
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-31 00:09:09 -0700

    MovementType

--- NWN.Framework.Lite/Enum/MovementType.cs
+++ NWN.Framework.Lite/Enum/MovementType.cs
@@ -1,11 +1,13 @@
 ﻿namespace NWN.Framework.Lite.Enum
 {
-    public enum MovementType
+    public class MovementType : NWEnumBase<int>
     {
-        Stationary,
-        Walk,
-        Run,
-        Sidestep,
-        WalkBackwards
+        public static readonly MovementType Stationary = new(0);
+        public static readonly MovementType Walk = new(1);
+        public static readonly MovementType Run = new(2);
+        public static readonly MovementType Sidestep = new(3);
+        public static readonly MovementType WalkBackwards = new(4);
+
+        public MovementType(int internalValue) : base(internalValue) { }
     }
 }
