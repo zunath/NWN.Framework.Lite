f4386493e10453a49fe1864269d44fd56a2a386a
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-30 20:14:02 -0700

    SpellSchoolType

--- NWN.Framework.Lite/Enum/SpellSchoolType.cs
+++ NWN.Framework.Lite/Enum/SpellSchoolType.cs
@@ -1,15 +1,17 @@
 namespace NWN.Framework.Lite.Enum
 {
-    public enum SpellSchoolType
+    public class SpellSchoolType : NWEnumBase<int>
     {
-        General = 0,
-        Abjuration = 1,
-        Conjuration = 2,
-        Divination = 3,
-        Enchantment = 4,
-        Evocation = 5,
-        Illusion = 6,
-        Necromancy = 7,
-        Transmutation = 8
+        public static readonly SpellSchoolType General = new(0);
+        public static readonly SpellSchoolType Abjuration = new(1);
+        public static readonly SpellSchoolType Conjuration = new(2);
+        public static readonly SpellSchoolType Divination = new(3);
+        public static readonly SpellSchoolType Enchantment = new(4);
+        public static readonly SpellSchoolType Evocation = new(5);
+        public static readonly SpellSchoolType Illusion = new(6);
+        public static readonly SpellSchoolType Necromancy = new(7);
+        public static readonly SpellSchoolType Transmutation = new(8);
+
+        public SpellSchoolType(int internalValue) : base(internalValue) { }
     }
 }
