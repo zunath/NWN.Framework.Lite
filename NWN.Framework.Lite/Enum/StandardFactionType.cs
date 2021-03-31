fd08113aa56a07cf43b051254a422a84fad1e7e6
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-30 20:03:39 -0700

    StandardFactionType

--- NWN.Framework.Lite/Enum/StandardFactionType.cs
+++ NWN.Framework.Lite/Enum/StandardFactionType.cs
@@ -1,10 +1,12 @@
 namespace NWN.Framework.Lite.Enum
 {
-    public enum StandardFactionType
+    public class StandardFactionType : NWEnumBase<int>
     {
-        Hostile,
-        Commoner,
-        Merchant,
-        Defender
+        public static readonly SubSkillType Hostile = new(0);
+        public static readonly SubSkillType Commoner = new(1);
+        public static readonly SubSkillType Merchant = new(2);
+        public static readonly SubSkillType Defender = new(3);
+
+        public StandardFactionType(int internalValue) : base(internalValue) { }
     }
 }
