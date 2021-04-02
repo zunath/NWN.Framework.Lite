1d5db64d5be2d95f7ca1840066381cf41f876c59
zunath <coolty3001@yahoo.com>
2021-04-02 15:24:46 -0400

    Swap ObjectType back to normal enum.

--- NWN.Framework.Lite/Enum/ObjectType.cs
+++ NWN.Framework.Lite/Enum/ObjectType.cs
@@ -1,26 +1,22 @@
+using System;
+
 namespace NWN.Framework.Lite.Enum
 {
-    public class ObjectType : NWEnumBase<int>
+    [Flags]
+    public enum ObjectType
     {
-        public static readonly ObjectType Creature = new(1);
-        public static readonly ObjectType Item = new(2);
-        public static readonly ObjectType Trigger = new(4);
-        public static readonly ObjectType Door = new(8);
-        public static readonly ObjectType AreaOfEffect = new(16);
-        public static readonly ObjectType Waypoint = new(32);
-        public static readonly ObjectType Placeable = new(64);
-        public static readonly ObjectType Player = new(69);
-        public static readonly ObjectType Store = new(128);
-        public static readonly ObjectType Encounter = new(256);
-        public static readonly ObjectType Tile = new(512);
-        public static readonly ObjectType All = new(32767);
-        public static readonly ObjectType Invalid = new(32767);
-
-        public ObjectType(int internalValue) : base(internalValue)
-        {
-
-        }
-
-        public static explicit operator ObjectType(int internalValue) => new(internalValue);
+        Creature = 1,
+        Item = 2,
+        Trigger = 4,
+        Door = 8,
+        AreaOfEffect = 16,
+        Waypoint = 32,
+        Placeable = 64,
+        Player = 69,
+        Store = 128,
+        Encounter = 256,
+        Tile = 512,
+        All = 32767,
+        Invalid = 32767
     }
 }
